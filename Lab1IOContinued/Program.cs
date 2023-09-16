ac22f0b03a74fe401f1c077469196c05752c82cf
YiFan <jiao1995cn@gmail.com>
2023-09-15 22:48:57 -0500

    lab1 done.

--- Lab1IOContinued/Program.cs
+++ Lab1IOContinued/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Lab 1!");
+﻿string inputFile = "C:\\Users\\jyf00\\Desktop\\oop\\Documents\\theMachineStops.txt";
+string directoryPath = Path.GetDirectoryName(Path.GetFullPath(inputFile));
+
+Directory.SetCurrentDirectory(directoryPath);
+
+string inputText;
+
+inputText = File.ReadAllText(inputFile);
+
+string modifiedText = inputText.Replace(".", " STOP");
+
+string outputFile = "TelegramCopy.txt";
+
+File.WriteAllText(outputFile, modifiedText);
+
